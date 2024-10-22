5946ecb4516bdbcd32e7f1647c79c2241f0ab9c3
Vitor Abreu <vitoryanfma@gmail.com>
2024-10-22 18:32:06 -0300

    feat(rxjs): creating a cold observable to call the api

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { noop, Observable } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,6 +11,22 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    //creating aa cold observable, to call API
+    const http$ = Observable.create(observer => {
+      fetch('/api/courses')
+        .then(data => data.json())
+        .then(body => {
+          observer.next(body);
+          observer.complete();
+        })
+        .catch(err => observer.error(err))
+    })
+
+    http$.subscribe(
+      courses => console.log(courses),
+      noop,
+      () => console.log('end')
+    )
   }
 
 }
