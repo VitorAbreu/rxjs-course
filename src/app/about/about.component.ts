8725aef34b67b30add67180dc4c9923c76adc189
Vitor <vitoryanfma@gmail.com>
2024-10-27 13:56:15 -0300

    feat(rxjs): seeing how concat works

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { concat, of } from 'rxjs';
 
 @Component({
   selector: 'about',
@@ -10,7 +11,16 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    const source1$ = of(1, 2, 3);
+    // const source1$ = interval(1000);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+
+    // concat is used to concat values of all Observables but it only subscribe the next value if
+    // the first has completed so if we change the source1$ to an interval, source2$ and source3$ won't emit
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$.subscribe(console.log);
   }
 
 }
