a7ffd507c2cab17f2f35720bd175c694e39ecfce
Vitor Abreu <vitoryanfma@gmail.com>
2024-11-09 13:28:43 -0300

    feat(rxjs): about subject

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,8 +1,5 @@
-import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
-import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
-import {createHttpObservable} from '../common/util';
-
+import { Component, OnInit } from '@angular/core';
+import { Subject } from 'rxjs';
 
 @Component({
     selector: 'about',
@@ -11,11 +8,31 @@ import {createHttpObservable} from '../common/util';
 })
 export class AboutComponent implements OnInit {
 
-    ngOnInit() {
+  ngOnInit() {
+    // when use subject:
+    // - previous methods not convenient
+    // - multicasting vales to multiple observables
+    // - source of data is not easy to transform to observable
+    // - subject is at the same time na observable  and na observer
+    // - we can emits values with subject but also combine it with other observables
+    // - subject mean to be a private, it's  no a good idea share the observer part to all the application, because we don't want to other parts of the project could call the methods next(), error(), complete()
+    // - when we want to share a subject we need to share using subject.asObservable();
+    // cons
+    // - we don't have a way to provide an unsubscribe logic to the observable
+    // - we have the risk of sharing accidentally the subject with other parts of the application which could 
+    // potentially take over the behavior by calling the next, complete or error
+    // - we should use it as little as possible
+    // - we should use the methods to transform what we need into a observable, using fromPromise(), from() etc
+    const subject = new Subject();
+    const series$ = subject.asObservable();
 
+    series$.subscribe(console.log);
 
-    }
-
+    subject.next(1);
+    subject.next(2);
+    subject.next(3);
+    subject.complete();
+  }
 
 }
 
